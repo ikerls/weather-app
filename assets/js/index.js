1f43c660603871082968a0d325159ae108383890
Ikerls <ikerlarrea150@gmail.com>
2021-10-08 14:22:43 +0200

    estructura inicial

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,20 @@
-require("dotenv").config();
+const settings = {
+  async: true,
+  crossDomain: true,
+  url: "http://api.openweathermap.org/data/2.5/weather?q=moscu&units=metric&appid=3c100b21bad8bdc82ba2121560f63892",
+  method: "GET",
+  headers: {},
+};
+
+$(function () {
+  $.ajax(settings).done(function (response) {
+    const {
+      weather,
+      main: { temp },
+    } = response;
+    console.log(weather[0].description);
+    console.log(temp);
+    $("h1").text(Math.round(temp));
+    $("p").text(weather[0].description);
+  });
+});
