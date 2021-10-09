ac72fd918d31a310b9bbe7386babd7b96dc9ad7c
Ikerls <ikerlarrea150@gmail.com>
2021-10-09 17:35:43 +0200

    mejoras en ajax

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,33 @@
-const settings = {
-  async: true,
-  crossDomain: true,
-  url: "http://api.openweathermap.org/data/2.5/weather?q=moscu&units=metric&appid=3c100b21bad8bdc82ba2121560f63892",
-  method: "GET",
-  headers: {},
-};
+function apiWeather(loc, lang = "en") {
+  return {
+    async: true,
+    crossDomain: true,
+    url:
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      loc +
+      "&units=metric&appid=3c100b21bad8bdc82ba2121560f63892&lang=" +
+      lang +
+      "",
+    method: "GET",
+    headers: {},
+  };
+}
 
 $(function () {
-  $.ajax(settings).done(function (response) {
-    const {
-      weather,
-      main: { temp },
-    } = response;
-    console.log(weather[0].description);
-    console.log(temp);
-    $("h1").text(Math.round(temp) + "°");
-    $("p").text(weather[0].description);
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+    let location = $("input").val();
+    $.ajax(apiWeather(location)).done(function (response) {
+      const {
+        weather,
+        main: { temp },
+        name,
+      } = response;
+      console.log(weather[0].description);
+      console.log(temp);
+      $("h2").text(name);
+      $("h1").text(Math.round(temp) + "°");
+      $("p").text(weather[0].description);
+    });
   });
 });
