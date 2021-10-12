5e69e59cb897bd97a1ef840e4943c60ff5aefe4e
Ikerls <ikerlarrea150@gmail.com>
2021-10-12 16:28:49 +0200

    mejora en los iconos y en el selector del icono

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,15 +19,48 @@ $(function () {
     let location = $("input").val();
     $.ajax(apiWeather(location)).done(function (response) {
       const {
-        weather,
+        weather: [{ description, icon }],
         main: { temp },
         name,
       } = response;
-      console.log(weather[0].description);
-      console.log(temp);
+      const main_icon = $("#main_icon");
+      if (main_icon.children().length > 0) {
+        main_icon.empty();
+      }
+      // selector del icono:
+      switch (icon) {
+        //Sunny
+        case "01d":
+          main_icon.append("<div class='sunny'></div>");
+          break;
+        //Partly cloudy
+        case "02d":
+          main_icon.append("<div class='partly_cloudy'></div>");
+          $("<div class='partly_cloudy__sun'></div>").appendTo(
+            "#main_icon .partly_cloudy"
+          );
+          $("<div class='partly_cloudy__cloud'></div>").appendTo(
+            "#main_icon .partly_cloudy"
+          );
+          break;
+        //Cloudy
+        case "03d" || "03n" || "04d" || "04n":
+          break;
+        //Rainy
+        case "09d" || "09n" || "10d" || "10n":
+          break;
+        //Thundery
+        case "11d" || "11n":
+          break;
+        default:
+          break;
+      }
+
+      //console.log(icon);
+      //console.log(temp);
       $("h2").text(name);
       $("h1").text(Math.round(temp) + "°");
-      $("p").text(weather[0].description);
+      $("p").text(description);
     });
   });
 });
