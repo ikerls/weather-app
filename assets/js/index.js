613d8edd8495a858f0b6693023928570e5233946
Ikerls <ikerlarrea150@gmail.com>
2021-10-12 17:48:13 +0200

    agregado los iconos de noche

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-function apiWeather(loc, lang = "en") {
+function apiWeather(loc, lang = "es") {
   return {
     async: true,
     crossDomain: true,
@@ -17,66 +17,70 @@ $(function () {
   $("form").on("submit", function (e) {
     e.preventDefault();
     let location = $("input").val();
-    $.ajax(apiWeather(location)).done(function (response) {
-      const {
-        weather: [{ description, icon }],
-        main: { temp },
-        name,
-      } = response;
-      const main_icon = $("#main_icon");
-      if (main_icon.children().length > 0) {
-        main_icon.empty();
-      }
-      console.log(icon);
-      // selector del icono:
-      switch (icon) {
-        //Sunny
-        case "01d":
-          main_icon.append("<div class='sunny'></div>");
-          break;
-        //Partly cloudy
-        case "02d":
-          main_icon.append("<div class='partly_cloudy'></div>");
-          $("<div class='partly_cloudy__sun'></div>").appendTo(
-            "#main_icon .partly_cloudy"
-          );
-          $("<div class='partly_cloudy__cloud'></div>").appendTo(
-            "#main_icon .partly_cloudy"
-          );
-          break;
-        //Cloudy
-        case "03d":
-        case "03n":
-        case "04d":
-        case "04n":
-          main_icon.append("<div class='cloudy'></div>");
-          break;
-        //Rainy
-        case "09d":
-        case "09n":
-        case "10d":
-        case "10n":
-          main_icon.append("<div class='rainy'></div>");
-          $("<div class='rainy__cloud'></div>").appendTo("#main_icon .rainy");
-          $("<div class='rainy__rain'></div>").appendTo("#main_icon .rainy");
-          break;
-        //Thundery
-        case "11d" || "11n":
-          main_icon.append("<div class='thundery'></div>");
-          $("<div class='thundery__cloud'></div>").appendTo(
-            "#main_icon .thundery"
-          );
-          $("<div class='thundery__rain'></div>").appendTo(
-            "#main_icon .thundery"
-          );
-          break;
-      }
+    $.ajax(apiWeather(location))
+      .done(function (response) {
+        const {
+          weather: [{ description, icon }],
+          main: { temp },
+          name,
+        } = response;
+        const main_icon = $("#main_icon");
+        if (main_icon.children().length > 0) {
+          main_icon.empty();
+        }
+        console.log(icon);
+        // selector del icono:
+        switch (icon) {
+          //Sunny
+          case "01d":
+            main_icon.append("<div class='sunny'></div>");
+            break;
+          //Partly cloudy
+          case "02d":
+            main_icon.append("<div class='partly_cloudy'></div>");
+            $("<div class='partly_cloudy__sun'></div>").appendTo(
+              "#main_icon .partly_cloudy"
+            );
+            $("<div class='partly_cloudy__cloud'></div>").appendTo(
+              "#main_icon .partly_cloudy"
+            );
+            break;
+          //Cloudy
+          case "03d":
+          case "03n":
+          case "04d":
+          case "04n":
+            main_icon.append("<div class='cloudy'></div>");
+            break;
+          //Rainy
+          case "09d":
+          case "09n":
+          case "10d":
+          case "10n":
+            main_icon.append("<div class='rainy'></div>");
+            $("<div class='rainy__cloud'></div>").appendTo("#main_icon .rainy");
+            $("<div class='rainy__rain'></div>").appendTo("#main_icon .rainy");
+            break;
+          //Thundery
+          case "11d" || "11n":
+            main_icon.append("<div class='thundery'></div>");
+            $("<div class='thundery__cloud'></div>").appendTo(
+              "#main_icon .thundery"
+            );
+            $("<div class='thundery__rain'></div>").appendTo(
+              "#main_icon .thundery"
+            );
+            break;
+        }
 
-      //console.log(icon);
-      //console.log(temp);
-      $("h2").text(name);
-      $("h1").text(Math.round(temp) + "°");
-      $("p").text(description);
-    });
+        //console.log(icon);
+        //console.log(temp);
+        $("h2").text(name);
+        $("h1").text(Math.round(temp) + "°");
+        $("p").text(description);
+      })
+      .fail(function () {
+        alert("ciudad no encontrada");
+      });
   });
 });
