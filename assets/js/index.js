586edeada09be86f6f88defd8d3708f971ecebf7
iker <ikerls@pm.me>
2021-10-13 12:16:19 +0200

    implementado la desestructuracion del objeto

--- assets/js/index.js
+++ assets/js/index.js
@@ -30,6 +30,65 @@ function apiForecast(lat, lon, lang = "en") {
 		headers: {},
 	};
 }
+// icon selector
+function iconSelector(location, icon) {
+	switch (icon) {
+		//Sunny
+		case "01d":
+			location.append("<div class='sunny'></div>");
+			break;
+		case "01n":
+			location.append("<div class='moon'></div>");
+			break;
+		//Partly cloudy
+		case "02d":
+			location.append("<div class='partly_cloudy_night'></div>");
+
+			$("<div class='partly_cloudy_night__moon'></div>").appendTo(
+				location.children()
+			);
+			$("<div class='partly_cloudy_night__cloud'></div>").appendTo(
+				location.children()
+			);
+			break;
+		case "02n":
+			location.append("<div class='partly_cloudy'></div>");
+			$("<div class='partly_cloudy__sun'></div>").appendTo(
+				location.children()
+			);
+			$("<div class='partly_cloudy__cloud'></div>").appendTo(
+				location.children()
+			);
+			break;
+		//Cloudy
+		case "03d":
+		case "03n":
+		case "04d":
+		case "04n":
+			main_icon.append("<div class='cloudy'></div>");
+			break;
+		//Rainy
+		case "09d":
+		case "09n":
+		case "10d":
+		case "10n":
+			main_icon.append("<div class='rainy'></div>");
+			$("<div class='rainy__cloud'></div>").appendTo("#main_icon .rainy");
+			$("<div class='rainy__rain'></div>").appendTo(location.children());
+			break;
+		//Thundery
+		case "11d":
+		case "11n":
+			main_icon.append("<div class='thundery'></div>");
+			$("<div class='thundery__cloud'></div>").appendTo(
+				location.children()
+			);
+			$("<div class='thundery__rain'></div>").appendTo(
+				location.children()
+			);
+			break;
+	}
+}
 
 $(function () {
 	$("form").on("submit", (e) => {
@@ -50,9 +109,15 @@ $(function () {
 
 				$.ajax(apiForecast(lat, lon))
 					.done((response) => {
-						// TODO: deconstruct response
-
-						console.log(response);
+						const { daily } = response;
+						console.log(daily);
+						for (const {
+							dt,
+							temp: { day },
+							weather: [{ description, icon }],
+						} of daily) {
+							console.log({ dt, description, icon, day });
+						}
 					})
 					.fail(() => {
 						alert("se ha producido un error;");
@@ -60,72 +125,7 @@ $(function () {
 				console.log({ lon, lat });
 				console.log(icon);
 				// selector del icono:
-				function iconSelector(location, icon) {
-					switch (icon) {
-						//Sunny
-						case "01d":
-							location.append("<div class='sunny'></div>");
-							break;
-						case "01n":
-							location.append("<div class='moon'></div>");
-							break;
-						//Partly cloudy
-						case "02d":
-							location.append(
-								"<div class='partly_cloudy_night'></div>"
-							);
 
-							$(
-								"<div class='partly_cloudy_night__moon'></div>"
-							).appendTo(location.children());
-							$(
-								"<div class='partly_cloudy_night__cloud'></div>"
-							).appendTo(location.children());
-							break;
-						case "02n":
-							location.append(
-								"<div class='partly_cloudy'></div>"
-							);
-							$(
-								"<div class='partly_cloudy__sun'></div>"
-							).appendTo(location.children());
-							$(
-								"<div class='partly_cloudy__cloud'></div>"
-							).appendTo(location.children());
-							break;
-						//Cloudy
-						case "03d":
-						case "03n":
-						case "04d":
-						case "04n":
-							main_icon.append("<div class='cloudy'></div>");
-							break;
-						//Rainy
-						case "09d":
-						case "09n":
-						case "10d":
-						case "10n":
-							main_icon.append("<div class='rainy'></div>");
-							$("<div class='rainy__cloud'></div>").appendTo(
-								"#main_icon .rainy"
-							);
-							$("<div class='rainy__rain'></div>").appendTo(
-								location.children()
-							);
-							break;
-						//Thundery
-						case "11d":
-						case "11n":
-							main_icon.append("<div class='thundery'></div>");
-							$("<div class='thundery__cloud'></div>").appendTo(
-								location.children()
-							);
-							$("<div class='thundery__rain'></div>").appendTo(
-								location.children()
-							);
-							break;
-					}
-				}
 				iconSelector(main_icon, icon);
 
 				$("h2").text(name);
