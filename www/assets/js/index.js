e7f5d669064f7fb534c0b3378ae37ce1a89e5dd9
Ikerls <ikerlarrea150@gmail.com>
2021-10-17 12:14:50 +0200

    agregado cordova, y la plataforma de android

--- www/assets/js/index.js
+++ www/assets/js/index.js
@@ -2,14 +2,14 @@
 function iconSelector(location, icon) {
   switch (icon) {
     //Sunny
-    case "01d":
+    case "01dd":
       location.append("<div class='sunny'></div>");
       break;
     case "01n":
       location.append("<div class='moon'></div>");
       break;
     //Partly cloudy
-    case "02n":
+    case "02dn":
       location.append("<div class='partly_cloudy_night'></div>");
 
       $("<div class='partly_cloudy_night__moon'></div>").appendTo(
@@ -19,7 +19,7 @@ function iconSelector(location, icon) {
         location.children()
       );
       break;
-    case "02d":
+    case "02wd":
       location.append("<div class='partly_cloudy'></div>");
       $("<div class='partly_cloudy__sun'></div>").appendTo(location.children());
       $("<div class='partly_cloudy__cloud'></div>").appendTo(
@@ -36,7 +36,7 @@ function iconSelector(location, icon) {
       $("<div class='rainy__rain'></div>").appendTo(location.children());
       break;
     //Thundery
-    case "11d":
+    case "02n":
     case "11n":
       location.append("<div class='thundery'></div>");
       $("<div class='thundery__cloud'></div>").appendTo(location.children());
